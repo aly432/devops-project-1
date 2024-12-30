556714a249150ad079df96c0c70f0a909485416f
aly432 <aly432@gmail.com>
2024-12-29 18:29:06 -0800

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -16,7 +16,7 @@ module "networking" {
 
 module "security_group" {
   source                     = "./security-groups"
-  ec2_sg_name                = "SG for EC2 for enabling port 5000"
+  ec2_sg_name                = "SG for EC2 to enable SSH(22) and HTTP(80)"
   vpc_id                     = module.networking.dev_proj_1_vpc_id
   public_subnet_cidr_block   = tolist(module.networking.public_subnet_cidr_block)
   ec2_sg_name_for_python_api = "SG for EC2 for enabling port 5000"
